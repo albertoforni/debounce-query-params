e273d5b1860310831c9b43bb8ae35b606f79bd7b
Alberto Forni <alberto.forn@gmail.com>
2015-06-08 10:15:23 +0200

    Add documentation to mixin

--- addon/mixins/debounce-query-params.js
+++ addon/mixins/debounce-query-params.js
@@ -1,15 +1,42 @@
 import Ember from 'ember';
 
+/**
+Inject this mixin in a route-driven controller in order to be able
+to update query params with a debounce.
+
+@class DebounceQueryParams
+*/
 export default Ember.Mixin.create({
+  /**
+  Setup the controller for debouncing query param by calling
+  setupDebounceQueryParams
+
+  @method init
+  */
   init: function() {
     this._super();
     this.setupDebounceQueryParams();
   },
 
+  /**
+  Update the underneath property hence the query param
+
+  @method updateQueryParam
+  @param param {String} the name of the property to update
+  @param value {String} the new value of the property to update
+  */
   updateQueryParam: function(param, value) {
     this.set(param, value);
   },
 
+  /**
+  Decounce the query param update
+
+  @method paramUpdate
+  @param context {Object} the name of the property to update
+  @param debounceParamName {String} the name of the property to update
+  in the format debounce_paramName
+  */
   paramUpdate: function(context, debounceParamName) {
     var originalParamName = debounceParamName.substr(9, debounceParamName.length);
     var originalParamValue = this.get(debounceParamName);
@@ -18,6 +45,11 @@ export default Ember.Mixin.create({
       originalParamValue, this.get(`debounceQueryParams.${originalParamName}`));
   },
 
+  /**
+  Create an observer for each of the properties in the debounceQueryParams obejct
+
+  @method setupDebounceQueryParams
+  */
   setupDebounceQueryParams: function() {
     var debounceQueryParams = this.get('debounceQueryParams');
 
